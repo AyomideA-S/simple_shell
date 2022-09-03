af9e1c0057c004db68aab8c02ecb420ecf36fe80
AyomideA-S <midesuperbest@gmail.com>
2022-09-03 11:19:53 +0100

    printenv with environ

---
+++ env-main.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * main - printenv program
+ *
+ * @ac: Argument Count
+ * @av: Argument Vector
+ * @env: Environment parameter
+ * Return: 0 (Success).
+ */
+int main(int ac, char **av, char **env)
+{
+	unsigned int i;
+	extern char **environ;
+
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+
+	return (0);
+}
